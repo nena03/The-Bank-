6f5d1a8d8b49de0013fcac18abd771fca091058a
nena03 <69650515+nena03@users.noreply.github.com>
2021-02-04 18:59:55 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace TheBank
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.account = new System.Windows.Forms.ComboBox();
+            this.action = new System.Windows.Forms.ComboBox();
+            this.amount = new System.Windows.Forms.TextBox();
+            this.submit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Account";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Transaction";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Amount";
+            // 
+            // account
+            // 
+            this.account.FormattingEnabled = true;
+            this.account.Location = new System.Drawing.Point(290, 38);
+            this.account.Name = "account";
+            this.account.Size = new System.Drawing.Size(121, 21);
+            this.account.TabIndex = 3;
+            // 
+            // action
+            // 
+            this.action.FormattingEnabled = true;
+            this.action.Location = new System.Drawing.Point(290, 81);
+            this.action.Name = "action";
+            this.action.Size = new System.Drawing.Size(121, 21);
+            this.action.TabIndex = 4;
+            // 
+            // amount
+            // 
+            this.amount.Location = new System.Drawing.Point(299, 129);
+            this.amount.Name = "amount";
+            this.amount.Size = new System.Drawing.Size(100, 20);
+            this.amount.TabIndex = 5;
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(168, 212);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(75, 23);
+            this.submit.TabIndex = 6;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.amount);
+            this.Controls.Add(this.action);
+            this.Controls.Add(this.account);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox account;
+        private System.Windows.Forms.ComboBox action;
+        private System.Windows.Forms.TextBox amount;
+        private System.Windows.Forms.Button submit;
+    }
+}
+
---
+++ SavingsAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBank
+{
+    class SavingsAccount : TheBank.BankAccount
+    {
+        private decimal m_interest = 0.01M;
+
+        public SavingsAccount(string owner) : base(owner)
+        { }
+       public override string ID
+        {
+            get { return this.m_owner + "-S"; }
+        }
+
+        public decimal Interest
+        {
+            get { return m_interest; }
+            set { m_interest = value; }
+        }
+
+        public decimal AddInterest()
+        {
+            this.Deposit(m_interest * this.Balance);
+            return this.Balance;
+        }
+    }
+}
